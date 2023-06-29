37510d74236acb5c02b70c4e930a858bf2e951cd
F.Kaya <fkbeys@gmail.com>
2023-06-29 10:45:27 +0200

    first spring

---
+++ src/main/java/com/kayaspring/kayaspring/KayaspringApplication.java
@@ -0,0 +1,13 @@
+package com.kayaspring.kayaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KayaspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KayaspringApplication.class, args);
+	}
+
+}
