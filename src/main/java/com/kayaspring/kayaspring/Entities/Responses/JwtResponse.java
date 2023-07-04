91308dc6c499bbd3e52b765de55e1214cc2b7845
F.Kaya <fkbeys@gmail.com>
2023-07-04 02:30:55 +0200

    unnecessary classes removed

--- src/main/java/com/kayaspring/kayaspring/Entities/Responses/JwtResponse.java
+++ src/main/java/com/kayaspring/kayaspring/Entities/Responses/JwtResponse.java
@@ -1,22 +1,20 @@
 package com.kayaspring.kayaspring.Entities.Responses;
 
-import java.util.List;
-
-public class JwtResponse {
-
-    public JwtResponse(String token, long id, String type, String username, List<String> roles) {
-        this.token = token;
-        this.type = type;
-        this.id = id;
-        this.username = username;
-        this.email = email;
-        this.roles = roles;
-    }
-
-    public String token;
-    public String type;
-    public long id;
-    public String username;
-    public String email;
-    public List<String> roles;
-}
+//
+//public class JwtResponse {
+//
+//    public JwtResponse(String token, long id, String type, String username, List<String> roles) {
+//        this.token = token;
+//        this.type = type;
+//        this.id = id;
+//        this.username = username;
+//        this.roles = roles;
+//    }
+//
+//    public String token;
+//    public String type;
+//    public long id;
+//    public String username;
+//    public String email;
+//    public List<String> roles;
+//}
