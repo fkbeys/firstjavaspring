29fbc998dbb958ec8d38b28f2b63fa55dc1beace
F.Kaya <fkbeys@gmail.com>
2023-07-04 13:35:07 +0200

    unnecessary classes removed

--- src/main/java/com/kayaspring/kayaspring/Entities/Responses/MessageResponse.java
+++
@@ -1,9 +0,0 @@
-package com.kayaspring.kayaspring.Entities.Responses;
-
-public class MessageResponse {
-    public MessageResponse(String message) {
-        this.message = message;
-    }
-
-    public String message ;
-}
